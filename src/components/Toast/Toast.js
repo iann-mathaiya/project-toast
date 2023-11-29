6402cd94818e480ef7ef45f0d3e8d2583f1a0942
Ian Mathaiya <iannmathaiya@gmail.com>
2023-11-29 09:26:48 +0300

    updated the toast component css styles based on variant selected

--- src/components/Toast/Toast.js
+++ src/components/Toast/Toast.js
@@ -1,38 +1,37 @@
-import React from 'react';
+import React from "react"
 import {
   AlertOctagon,
   AlertTriangle,
   CheckCircle,
   Info,
   X,
-} from 'react-feather';
+} from "react-feather"
 
-import VisuallyHidden from '../VisuallyHidden';
+import VisuallyHidden from "../VisuallyHidden"
 
-import styles from './Toast.module.css';
+import styles from "./Toast.module.css"
 
 const ICONS_BY_VARIANT = {
   notice: Info,
   warning: AlertTriangle,
   success: CheckCircle,
   error: AlertOctagon,
-};
+}
 
-function Toast() {
+function Toast({children, variant}) {
+  const Icon = ICONS_BY_VARIANT[variant]
   return (
-    <div className={`${styles.toast} ${styles.notice}`}>
+    <div className={`${styles.toast} ${styles[variant]}`}>
       <div className={styles.iconContainer}>
-        <Info size={24} />
+        <Icon size={24} aria-hidden='true' />
       </div>
-      <p className={styles.content}>
-        16 photos have been uploaded
-      </p>
+      <p className={styles.content}>{children}</p>
       <button className={styles.closeButton}>
         <X size={24} />
         <VisuallyHidden>Dismiss message</VisuallyHidden>
       </button>
     </div>
-  );
+  )
 }
 
-export default Toast;
+export default Toast
